7783bd108f0fc70bc6323e29a640aa6025a91977
Miguel Medina <miguel@medinagtz.com>
2023-02-28 22:09:23 -0600

    add basic scene

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,8 +1,16 @@
-import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import {waitFor} from '@motion-canvas/core/lib/flow';
+import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
+import { all, waitFor } from "@motion-canvas/core/lib/flow";
+import { createRef } from "@motion-canvas/core/lib/utils";
+import { Circle } from "@motion-canvas/2d/lib/components";
 
 export default makeScene2D(function* (view) {
   // Create your animations here
-
-  yield* waitFor(5);
+  const myCircle = createRef<Circle>();
+  view.add(
+    <Circle ref={myCircle} x={-300} width={240} height={240} fill="#e13238" />
+  );
+  yield* all(
+    myCircle().position.x(300, 1).to(-300, 1),
+    myCircle().fill("#e6a700", 1).to("#e13238", 1)
+  );
 });
